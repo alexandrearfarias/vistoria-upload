f949c5b9cecd964d0637a4bdbbe0cffff4a20888
Alexandre Farias <alexandrearfarias@gmail.com>
2026-05-13 16:03:45 -0300

    ADD: criação de configurações para aplicativo web (PWA)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { registerSW } from 'virtual:pwa-register';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import App from './App.jsx';
+
+registerSW({ immediate: true });
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,48 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'favicon.svg',
+      ],
+      devOptions: {
+        enabled: true,
+      },
+
+      manifest: {
+        name: 'Organizador de Drive',
+        short_name: 'OrgDrive',
+        description: 'App para organização de vistorias técnicas dentro do Drive',
+        theme_color: '#3e94f7',
+        background_color: '#ffffff',
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
 })
